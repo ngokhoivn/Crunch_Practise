eedd0962f62fdae4821e6c39bebd1287f62360ee
Ngo Khoi <ngokhoivn@gmail.com>
2025-08-02 12:01:29 +0700

    Bai_6

---
+++ Source/Crunch/Private/Animations/CAnimInstance.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animations/CAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UCAnimInstance::NativeInitializeAnimation()
+{
+	OwnerCharacter = Cast<ACharacter>(TryGetPawnOwner());
+	if (OwnerCharacter)
+	{
+		OwnerMovementComp = OwnerCharacter->GetCharacterMovement();
+		Speed = OwnerCharacter->GetVelocity().Length();
+	}
+}
+
+void UCAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	if (OwnerCharacter)
+	{
+		Speed = OwnerCharacter->GetVelocity().Length();
+	}
+}
+
+void UCAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+}
--- Source/Crunch/Private/Animations/CAnimInstance.h
+++ Source/Crunch/Private/Animations/CAnimInstance.h
@@ -4,7 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
-#include "UCAnimInstance.generated.h"
+#include "CAnimInstance.generated.h"
 
 
 class ACharacter;
@@ -13,7 +13,7 @@ class UCharacterMovementComponent;
  * 
  */
 UCLASS()
-class UUCAnimInstance : public UAnimInstance
+class UCAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 	
@@ -21,10 +21,22 @@ public:
 	virtual void NativeInitializeAnimation() override;
 	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintThreadSafe))
+	FORCEINLINE float GetSpeed() const { return Speed; }
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintThreadSafe))
+	FORCEINLINE bool IsMoving() const { return Speed != 0; }
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintThreadSafe))
+	FORCEINLINE bool IsNotMoving() const { return Speed == 0; }
 private:
+	float Speed;
+
 	UPROPERTY()
 	class ACharacter* OwnerCharacter;
 
 	UPROPERTY()
 	class UCharacterMovementComponent* OwnerMovementComp;
+
 };
